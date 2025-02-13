c5fafd22e1559466e957c202af61e205d8774048
ViniJesus <vini.silva1342@gmail.com>
2025-02-13 19:49:33 -0300

    Sessao contato e botao scroll home

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
-const buttonProduct = document.querySelector(".button-produtos")
+const buttonProduct = document.querySelector(".button-produtos");
 
-buttonProduct.addEventListener("click", ()=>{
-    window.location.href = "pedido.html"
-})
+buttonProduct.addEventListener("click", () => {
+  window.location.href = "pedido.html";
+});
+
+window.document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("scroll", () => {
+    const botao = document.getElementById("botao");
+
+    if (window.scrollY > 300) {
+      botao.style.display = "block";
+      botao.addEventListener("click", () => {
+        window.location.href = "#Home";
+      });
+    } else {
+      botao.style.display = "none";
+    }
+  });
+});
