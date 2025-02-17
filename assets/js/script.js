72cc5bcea2727e68e38047041d8c06b19d9886c3
ViniJesus <vini.silva1342@gmail.com>
2025-02-17 16:38:59 -0300

    Tela Cardapio e Estilização tela de pedido

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,25 @@
-const buttonProduct = document.querySelector(".button-produtos");
-
-buttonProduct.addEventListener("click", () => {
-  window.location.href = "pedido.html";
-});
+const botaoProduto = document.querySelector(".button-produtos");
+const botaoCardapio = document.querySelector("#cardapio");
 
 window.document.addEventListener("DOMContentLoaded", () => {
-  window.addEventListener("scroll", () => {
-    const botao = document.getElementById("botao");
-
-    if (window.scrollY > 300) {
-      botao.style.display = "block";
-      botao.addEventListener("click", () => {
-        window.location.href = "#Home";
-      });
-    } else {
-      botao.style.display = "none";
-    }
+  botaoProduto.addEventListener("click", () => {
+    window.location.href = "pedido.html";
   });
+});
+
+botaoCardapio.addEventListener("click", () => {
+  window.location.href = "cardapio.html";
+});
+
+window.addEventListener("scroll", () => {
+  const botao = document.getElementById("botao");
+
+  if (window.scrollY > 300) {
+    botao.style.display = "block";
+    botao.addEventListener("click", () => {
+      window.location.href = "#Home";
+    });
+  } else {
+    botao.style.display = "none";
+  }
 });
